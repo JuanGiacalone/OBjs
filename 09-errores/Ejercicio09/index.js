f466b79b7789cfe94ad660e08fe4c5f9018a8547
Juan Giacalone <juanpablogiacalone8@gmail.com>
2022-08-22 21:16:52 +0200

    fin de sesion09

---
+++ 09-errores/Ejercicio09/index.js
@@ -0,0 +1,15 @@
+// Crea un nuevo proyecto de Node
+
+// - Instala la dependencia Winston
+
+// - En el archivo index.js crea una función que devuelva un error con un mensaje personalizado
+
+// - Registra el error en un archivo a través de un try...catch
+
+const logger = require("./logger");
+
+try {
+  console.log(fallo);
+} catch (error) {
+  logger.error(`${error}`);
+}
---
+++ 09-errores/index.js
@@ -0,0 +1,11 @@
+const logger = require("./logger");
+
+//logger.log("logger log");
+
+logger.info("logger info");
+
+logger.warn(" logger warn");
+
+logger.error(" logger error");
+
+logger.debug(" mensaje debug");
